4850fd16290eeb51cad8a21fd173cd444b1de5b4
backspacer303 <nikola.peric303@gmail.com>
2020-11-20 19:45:26 +0100

    Hello Cube!
    Setting up 3D scene: model, view and projection matrices are now set

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@
 #include <iostream>
 #include "Shader.hpp"
 
+#define WIDTH 800
+#define HEIGHT 600
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 
 void ThreadWindowFunction(){
     //create window object
-    GLFWwindow* window = glfwCreateWindow(800,600,"Set The Stage!", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH,HEIGHT,"Set The Stage!", NULL, NULL);
     //GLFWwindow* window2 = glfwCreateWindow(800,600,"Set The Stage!", NULL, NULL);
     if(window == NULL){
         std::cout << "Window creation failed!" << std::endl;
@@ -37,12 +40,52 @@ void ThreadWindowFunction(){
     //read shader scripts, compile them, create program object and link shaders to it
     Shader shader("../shader.vertex", "../shader.fragment");
 
-    //define positions and color of triangle vertices
+    glEnable(GL_DEPTH_TEST);
+
+    //define positions and color of cube vertices
     float vertices[] = {
-            // positions         // colors
-            0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
-            -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,
-            0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f
+             //position           //color
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f
     };
 
     //create and bind buffers
@@ -69,24 +112,25 @@ void ThreadWindowFunction(){
         //screen clearing color
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         //clear the screen
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //use our program object with linked shaders
         shader.use();
         glBindVertexArray(VAO);
 
-        //GLM: tiangle rotation
+        //GLM: set up model, view and projection matrices
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime()*glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
         shader.setModelUniform("model", model);
         shader.setViewUniform("view", view);
-        //shader.setProjectionUniform("projection", projection);
+        shader.setProjectionUniform("projection", projection);
 
         //draw triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         //double buffering: swap the front and back buffers
         glfwSwapBuffers(window);
@@ -114,14 +158,11 @@ int main() {
     //set up core-profile mode
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    //TODO dva prozora u dve niti:
-    // mozda je ovo dobro mesto da se naprave dve niti a kod ispod da bude zajednicki za obe
-
     std::thread window1(ThreadWindowFunction);
-    std::thread window2(ThreadWindowFunction);
+    //std::thread window2(ThreadWindowFunction);
 
     window1.join();
-    window2.join();
+    //window2.join();
 
     return 0;
 }
