9e4d627c1e012302330d964775d046991a0d021e
backspacer303 <nikola.peric303@gmail.com>
2020-11-29 18:18:50 +0100

    Endless path; errors
    - aplication now renders endless path with obstacle on it
    - implementation of error functions and error macro

--- main.cpp
+++ main.cpp
@@ -14,13 +14,33 @@
 #define WIDTH 800
 #define HEIGHT 600
 
+#define ASSERT(x) if(!(x)) exit(EXIT_FAILURE);
+#define GLCall(f) \
+    do{           \
+        GL_ClearError(); \
+        f;        \
+        ASSERT(GL_LogError(__FILE__, __LINE__))\
+    }while(0)
+
+static void GL_ClearError(){
+    while (glGetError() != GL_NO_ERROR);
+}
+static bool GL_LogError(const char* file, int line){
+    if (GLenum error = glGetError()){
+        std::cout << "[OpenGL Error] (0x0" << std::hex << error << std::dec << "): "  <<
+                        file << " line: " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 
 void ThreadWindowFunction(){
     //create window object
-    GLFWwindow* window = glfwCreateWindow(WIDTH,HEIGHT,"Set The Stage!", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH,HEIGHT,"Set The Stage of Etrenal Solitude!", NULL, NULL);
     if(window == NULL){
         std::cout << "Window creation failed!" << std::endl;
         glfwTerminate();
@@ -91,6 +111,7 @@ void ThreadWindowFunction(){
             -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f
     };
 
+
     //create vertex buffer and vertex array and set vertex buffer layout
     VertexArray va;
     auto* vb = new VertexBuffer(vertices, sizeof(vertices));
@@ -98,6 +119,11 @@ void ThreadWindowFunction(){
     layout->Push<float>(3); //first 3 floats represent vertex coordinates
     layout->Push<float>(3); //next 3 floats represent color value for corresponding vertex
     va.AddBuffer(*vb, *layout); //set layout of vertex buffer
+
+
+    float plate1_trans = 0;
+    float plate2_trans = -1;
+    float ghost_border = 1.5;
 
     //render loop
     while(!glfwWindowShouldClose(window)){
@@ -109,23 +135,56 @@ void ThreadWindowFunction(){
 
         //use our program object with linked shaders
         shader.use();
-        //bind vertex array
-        va.Bind();
 
-        //GLM: set up model, view and projection matrices
-        glm::mat4 model = glm::mat4(1.0f);
+        //GLM: set up view and projection matrices
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view  = glm::lookAt(glm::vec3(0,1,4), glm::vec3(0,0,0), glm::vec3(0,1,0));
         projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
         //set corresponding uniforms in vertex shader
-        shader.setModelUniform("model", model);
         shader.setViewUniform("view", view);
         shader.setProjectionUniform("projection", projection);
 
-        //draw triangle
+
+        //bind vertex array
+        va.Bind();
+
+        if(plate1_trans >= ghost_border)
+            plate1_trans = -1 + plate2_trans;
+        if(plate2_trans >= ghost_border)
+            plate2_trans = -1 + plate1_trans;
+
+
+
+        //draw first path plate
+        glm::mat4 model = glm::mat4(1.0f);
+        //model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(4, 0.125, 100));
+        model = glm::translate(model, glm::vec3(0, -0.5, -0.5+plate1_trans));
+        shader.setModelUniform("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        //draw middle obstacle relative to the plate [no reset of model matrix!]
+        model = glm::scale(model, glm::vec3(1.0/4, 1.0/0.125, 1.0/100));
+        model = glm::translate(model, glm::vec3(0, 0.5, 0));
+        shader.setModelUniform("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        //draw second path plate
+        model = glm::mat4(1.0f);
+        model = glm::scale(model, glm::vec3(4, 0.125, 100));
+        model = glm::translate(model, glm::vec3(0, -0.5, -0.5+plate2_trans));
+        shader.setModelUniform("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        //draw middle obstacle relative to the plate [no reset of model matrix!]
+        model = glm::scale(model, glm::vec3(1.0/4, 1.0/0.125, 1.0/100));
+        model = glm::translate(model, glm::vec3(0, 0.5, 0));
+        shader.setModelUniform("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        plate1_trans += 0.002;
+        plate2_trans += 0.002;
 
         //double buffering: swap the front and back buffers
         glfwSwapBuffers(window);
